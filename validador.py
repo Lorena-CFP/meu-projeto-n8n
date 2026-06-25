c2be0e838f780b404cd1af24a50b68ac68e6d676
Lorena Cordeiro <lorenacordeiro.fp@gmail.com>
2026-06-25 12:31:22 -0300

    Codigo Base

---
+++ validador.py
@@ -0,0 +1,15 @@
+import re
+
+def validar_email(email):
+    # Checa se o formato do e-mail é válido
+    padrao = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    if re.match(padrao, email):
+        return True
+    return False
+
+# Exemplo de teste
+email_teste = "suporte@n8n.io"
+if validar_email(email_teste):
+    print(f"O e-mail '{email_teste}' é válido!")
+else:
+    print(f"O e-mail '{email_teste}' é inválido.")
