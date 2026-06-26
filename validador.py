0cddb001b50ba57516eb8b2f84279b3f08a35ccd
Lorena Cordeiro <lorenacordeiro.fp@gmail.com>
2026-06-26 11:15:44 -0300

    TESTE FINAL

--- validador.py
+++ validador.py
@@ -8,7 +8,7 @@ def validar_email(email):
     return False
 
 # Exemplo de teste
-email_teste = "testebomdiafamlia232323.com"
+email_teste = "Batatinhafrita1234567@gmail.com"
 if validar_email(email_teste):
     print(f"O e-mail '{email_teste}' é GOLLLLLLLLLLL!")
 else:
