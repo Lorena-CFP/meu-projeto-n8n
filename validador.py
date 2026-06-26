5a08602a754f87a1f90182b515e0f3e570b6f0cd
Lorena Cordeiro <lorenacordeiro.fp@gmail.com>
2026-06-26 11:37:58 -0300

    Fiz uma alteração no email

--- validador.py
+++ validador.py
@@ -8,7 +8,7 @@ def validar_email(email):
     return False
 
 # Exemplo de teste
-email_teste = "Batatinhafrita1234567@gmail.com"
+email_teste = "batatatatatatatatttatatata@gmail.com"
 if validar_email(email_teste):
     print(f"O e-mail '{email_teste}' é GOLLLLLLLLLLL!")
 else:
